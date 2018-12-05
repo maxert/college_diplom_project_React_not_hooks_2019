37e75f9e34b5d843ad806c288de7a160c620bb78
maxert <max97-12@ukr.net>
2018-12-05 21:19:27 +0200

    update

---
+++ src/header.js
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+import "./App.css";
+
+class Header extends Component {
+  render() {
+    return (
+        <header className="header_menu">
+          <div className="container">
+            <div className="row">
+              <div className="col-md-6">123</div>
+            </div>
+          </div>
+        </header>
+    );
+  }
+}
+
+export default Header;
