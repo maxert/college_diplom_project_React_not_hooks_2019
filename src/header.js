3a2a492eb2f4e05e57817534bf50fd3fe437f05f
maxert <max97-12@ukr.net>
2018-12-05 21:34:57 +0200

    update bootstrap

--- src/header.js
+++ src/header.js
@@ -1,16 +1,15 @@
 import React, { Component } from "react";
-import "./App.css";
 
 class Header extends Component {
   render() {
     return (
-        <header className="header_menu">
-          <div className="container">
-            <div className="row">
-              <div className="col-md-6">123</div>
-            </div>
+      <header className="header_menu">
+        <div className="container">
+          <div className="row">
+            <div className="col-md-6">123</div>
           </div>
-        </header>
+        </div>
+      </header>
     );
   }
 }
--- src/index.js
+++ src/index.js
@@ -3,10 +3,10 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import * as serviceWorker from './serviceWorker';
 import Header from './header';
+import 'bootstrap';
+import 'bootstrap/js/dist/util';
+import 'bootstrap/js/dist/dropdown';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.render(<Header />, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
 serviceWorker.unregister();
