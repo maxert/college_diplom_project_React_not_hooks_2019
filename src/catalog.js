273be17758d8f1723f8ffffeb2da7988ae0736e7
maxert <max97-12@ukr.net>
2019-01-20 23:50:07 +0200

    update

--- src/catalog.js
+++ src/catalog.js
@@ -5,29 +5,71 @@ class Catalog extends Component {
     super(props);
     this.state = {
       massive: [],
-      textURl: 'http://localhost:1337/Items',
-      value: 'http://localhost:1337/Items?top=1',
+      massiveTwo: [],
+      value: 'http://localhost:1337/Items?_sort=top:ASC',
+      valueFilter: 'http://localhost:1337/Items?_sort=top:ASC',
+    };
+    this.sort = {
+      textURl: 'http://localhost:1337/Items?_sort=price:DESC&SortItems=0',
+      constURL: 'http://localhost:1337/Items',
+      isFalse: false,
     };
 
     this.handleChange = this.handleChange.bind(this);
+    this.handleChangeFilterTovar = this.handleChangeFilterTovar.bind(this);
     // Request API.
   }
   handleChange(event) {
     this.setState({ value: event.target.value });
-    if (event.target.value === 'По новинкам') {
-      this.state.textURl = 'http://localhost:1337/Items?_sort=new';
-    } else if (event.target.value === 'По популярности') {
-      this.state.textURl = 'http://localhost:1337/Items?_sort=top';
-    } else if (event.target.value === 'По возростанию цены') {
-      this.state.textURl = 'http://localhost:1337/Items?_sort=price:DESC';
-    } else {
-      this.state.textURl = 'http://localhost:1337/Items?_sort=price:ASC';
+    for (var i = 0; i < this.state.massive.length; i++) {
+      if (event.target.value === 'По новинкам') {
+        this.sort.textURl = this.sort.constURL + '?_sort=new&SortItems=' + this.state.massive[i].SortItems;
+      } else if (event.target.value === 'По популярности') {
+        this.sort.textURl = this.sort.constURL + '?_sort=top&SortItems=' + this.state.massive[i].SortItems;
+      } else if (event.target.value === 'По возростанию цены') {
+        this.sort.textURl = this.sort.constURL + '?_sort=price:DESC&SortItems=' + this.state.massive[i].SortItems;
+      } else {
+        this.sort.textURl = this.sort.constURL + '?_sort=price:ASC&SortItems=' + this.state.massive[i].SortItems;
+      }
     }
     this.componentDidMount();
   }
+  handleChangeFilterTovar(event) {
+    this.setState({ valueFilter: event.target.value });
+    for (var i = 0; i < this.state.massiveTwo.length; i++) {
+      if (Number(event.target.value) === this.state.massiveTwo[i].SortItems) {
+        this.sort.textURl = this.sort.constURL + '?_sort=top&SortItems=' + this.state.massiveTwo[i].SortItems;
+      }
+    }
+    this.componentDidMount();
+  }
+  filterfill() {
+    var filter_TitleName = document.getElementsByClassName('filter_TitleName')[0];
+    if (filter_TitleName) {
+      if (this.sort.isFalse === false) {
+        this.sort.isFalse = true;
+        for (var j = 0; j < this.state.massiveTwo.length; j++) {
+          var option = document.createElement('option');
+          for (var r = 0; r < this.state.massiveTwo.length; r++) {
+            if (j === this.state.massiveTwo[r].SortItems) {
+              filter_TitleName.appendChild(option);
+            }
+          }
+        }
+        for (var i = 0; i < filter_TitleName.children.length; i++) {
+          filter_TitleName.children[i].value = i;
+          for (var k = 0; k < this.state.massiveTwo.length; k++) {
+            if (Number(filter_TitleName.children[i].value) === this.state.massiveTwo[k].SortItems) {
+              filter_TitleName.children[i].innerHTML = this.state.massiveTwo[k].TypeTitle;
+            }
+          }
+        }
+      }
+    }
+  }
   componentDidMount() {
     axios
-      .get(this.state.textURl)
+      .get(this.sort.textURl)
       .then(response => {
         // Handle success.
         this.setState({ massive: response.data });
@@ -37,7 +79,24 @@ class Catalog extends Component {
         console.log('An error occurred:', error);
       });
   }
+  componentDidMountTwo() {
+    if (this.sort.isFalse === false) {
+      axios
+        .get(this.sort.constURL)
+        .then(response => {
+          // Handle success.
+          this.setState({ massiveTwo: response.data });
+          this.sort.isFalse = true;
+        })
+        .catch(error => {
+          // Handle error.
+          console.log('An error occurred:', error);
+        });
+    }
+  }
   render() {
+    this.componentDidMountTwo();
+    this.filterfill();
     const itemProduct = this.state.massive.map((massive, i) => (
       <div className="item_block m-2 p-3" key={i}>
         <img src={'../img/' + massive.image.name} className="img-fluid mt-2 mb-2" alt="Картинка" />
@@ -49,11 +108,19 @@ class Catalog extends Component {
         </div>
       </div>
     ));
+
     return (
       <div className="section_center catalog">
         <div className="container d-flex flex-row">
           <div className="col-md-3 ml-1 mr-1">
             <div className="col-md-12 mt-2 text_catalog">Фильтр</div>
+            <div className="filter_tovar">
+              <select
+                className="filter_TitleName"
+                value={this.state.valueFilter}
+                onChange={this.handleChangeFilterTovar}
+              />
+            </div>
           </div>
           <div className="col-md-9 ml-1 mr-1">
             <div className="col-md-12 mt-2 text_catalog">
